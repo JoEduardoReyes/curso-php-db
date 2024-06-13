507113c9d5f5e0b715e975bc87e26d1d302cbbd6
Eduardo Reyes <joeduardoreyes@outlook.com>
2024-06-13 09:01:35 -0600

    Evitando SQL Injection con bindParam

--- app/Controllers/IncomesController.php
+++ app/Controllers/IncomesController.php
@@ -4,54 +4,73 @@ namespace App\Controllers;
 
 use Database\MySQLi\Connection;
 
-class IncomesController {
+class IncomesController
+{
 
   /**
    * Muestra una lista de este recurso
    */
-  public function index() {}
+  public function index()
+  {
+  }
 
   /**
    * Muestra un formulario para crear un nuevo recurso
    */
-  public function create() {}
+  public function create()
+  {
+  }
 
   /**
    * Guarda un nuevo recurso en la base de datos
    */
-  public function store($data) {
+  public function store($data): void
+  {
+    $payment_method = $data['payment_method'];
+    $type = $data['type'];
+    $date = $data['date'];
+    $amount = $data['amount'];
+    $description = $data['description'];
 
     $connection = Connection::getInstance()->get_database_instance();
 
-    $connection->query("INSERT INTO incomes (payment_method, type, date, amount, description) VALUES(
-            {$data['payment_method']},
-            {$data['type']},
-            '{$data['date']}',
-            {$data['amount']},
-            '{$data['description']}'
-        );");
+    $stmt = $connection->prepare("INSERT INTO incomes (payment_method, type, date, amount, description) VALUES(?,?,?,?,?);");
+
+    $stmt->bind_param("iisds", $payment_method, $type, $date, $amount, $description);
+
+    $stmt->execute();
+
+    echo "Se han insertado {$stmt->affected_rows} filas en la base de datos.";
 
   }
 
   /**
    * Muestra un único recurso especificado
    */
-  public function show() {}
+  public function show()
+  {
+  }
 
   /**
    * Muestra el formulario para editar un recurso
    */
-  public function edit() {}
+  public function edit()
+  {
+  }
 
   /**
    * Actualiza un recurso específico en la base de datos
    */
-  public function update() {}
+  public function update()
+  {
+  }
 
   /**
    * Elimina un recurso específico de la base de datos
    */
-  public function destroy() {}
+  public function destroy()
+  {
+  }
 
 }
 
--- index.php
+++ index.php
@@ -11,6 +11,6 @@ $incomes_controller->store([
   "payment_method" => PaymentMethodEnum::BankAccount->value,
   "type" => IncomeTypeEnum::Salary->value,
   "date" => date("Y-m-d H:i:s"), // 2022-06-24 15:06:45
-  "amount" => 1000000,
+  "amount" => 84351,
   "description" => "Pago de mi salario por mi arduo y muy bien trabajo :D"
 ]);
